9406281f164c784fa4751328e03168e6942e89ea
ColonelBucket8 <freedomterran89@gmail.com>
2022-05-17 21:27:54 +0800

    added squeeze

--- increment-decrement/squeeze.c
+++ increment-decrement/squeeze.c
@@ -8,13 +8,16 @@ void squeeze_compare (char str1[], char str2[]);
 int main () {
 
     char str1[MAXSTR] = "abesfasdf";
+    char str_to_compare[MAXSTR] = "dabesf";
     char str2[MAXSTR] = "aasrwerasdfasdf";
     int character = 'a';
 
     printf("Original input: %s\n", str2);
     squeeze(str2, character);
+    squeeze_compare(str1, str_to_compare);
 
-    printf("Output: %s\n", str2);
+    printf("Output of squeeze: %s\n", str2);
+    printf("Output of squeeze_to_compare: %s\n", str1);
 
     return 0;
 }
@@ -33,4 +36,13 @@ void squeeze (char str[], int c) {
 
 void squeeze_compare (char str1[], char str2[]) {
     int i, j, k;
+
+    for (k = 0; str2[k] != '\0'; k++) {
+      for (i = j = 0; str1[i] != '\0'; i++) {
+        if (str1[i] != str2[k]) {
+          str1[j++] = str1[i];
+        }
+      }
+      str1[j] = '\0';
+    }
 }
