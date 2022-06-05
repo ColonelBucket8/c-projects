f7d1437be6e347c260609dcce0ab604fc472abb1
ColonelBucket8 <freedomterran89@gmail.com>
2022-06-05 16:50:37 +0800

    added struct

---
+++ struct/struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
+
+int main() {
+
+  struct Student student1;
+  student1.age = 22;
+  student1.gpa = 3.2;
+  strcpy(student1.name, "Jim");
+  strcpy(student1.major, "Business");
+
+
+  struct Student student2;
+  student1.age = 20;
+  student1.gpa = 2.5;
+  strcpy(student1.name, "Pam");
+  strcpy(student1.major, "Art");
+
+  printf("%s", student1.name);
+  printf("%s", student2.name);
+
+   return 0;
+}
+
+
